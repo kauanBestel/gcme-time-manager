49051b89a074a616cd82f69b6c57b2d1562e48c9
KauanBestell <kauan.bestell@gmail.com>
2025-04-25 11:39:44 -0300

    WIP: mapper for Mdo has ben finished, the implementation on Mdo service is stil pending

--- src/main/java/br/com/gcme/gcme/mapper/MdoMapper.java
+++ src/main/java/br/com/gcme/gcme/mapper/MdoMapper.java
@@ -1,5 +1,7 @@
 package br.com.gcme.gcme.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
@@ -10,7 +12,15 @@ import br.com.gcme.gcme.entity.Mdo;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MdoMapper {
 
-    Mdo map(MdoRequest mdoRequest, MdoResponse mdoResponse);
-    Mdo map(MdoRequest mdorequest);
+    // de DTO de entrada (POST/PUT) para entity
+    Mdo toEntity(MdoRequest mdorequest);
+
+
+    // de entidade para DTO de saida (GET)
+    MdoResponse toResponse(Mdo mdo);
+
+    // lista de entidades para lista de DTO de saida
+    List<MdoResponse> toResponseList(List<Mdo> mdoList);
+    
    
 }
